8d0aed0310c3d1201d246e69e1401ac52df2da43
Hannah <118190915+hannadzenis@users.noreply.github.com>
2023-08-08 15:52:53 +0200

    feat: developed form + PHP

--- src/js/main.js
+++ src/js/main.js
@@ -55,19 +55,115 @@ openBurger.addEventListener('click', closeBurger);
 
 /* POP-UP */
 
-const itemCards = document.querySelectorAll('.element-animation')
-const overlay = document.querySelector('.overlay')
+const itemCardDeloitte = document.querySelector('.element-animation--1')
+const itemCardFreelance = document.querySelector('.element-animation--2')
+const overlayDel = document.querySelector('.deloitte')
+const overlayFree = document.querySelector('.freelance')
 const body = document.querySelector('body')
 
-itemCards.forEach((item) => {
-  item.addEventListener('click', ()=>{
-    overlay.setAttribute('style', 'display: block');
-    body.setAttribute('style', 'overflow: hidden')
+
+itemCardDeloitte.addEventListener('click', ()=>{
+  overlayDel.setAttribute('style', 'display: block');
+  body.setAttribute('style', 'overflow: hidden')
+})
+
+itemCardFreelance.addEventListener('click', ()=>{
+  overlayFree.setAttribute('style', 'display: block');
+  body.setAttribute('style', 'overflow: hidden')
+})
+
+
+const closePopUp = document.querySelectorAll('.pop-up__content__close')
+
+closePopUp.forEach((item)=>{
+  item.addEventListener('click', () => {
+    overlayFree.setAttribute('style', 'display: none');
+    overlayDel.setAttribute('style', 'display: none');
+    body.setAttribute('style', 'overflow: visible')
   })
 })
 
-const closePopUp = document.querySelector('.pop-up__content__close')
-closePopUp.addEventListener('click', () => {
-  overlay.setAttribute('style', 'display: none');
-  body.setAttribute('style', 'overflow: visible')
+/* FORM */
+
+const inputs = document.querySelectorAll('input, textarea');
+const form = document.getElementById('form');
+
+inputs.forEach((item)=>{
+  item.setAttribute('required', '');
 })
+
+form.addEventListener('submit', formSend);
+
+const submitFormButton = document.getElementById('submit');
+console.log(submitFormButton)
+submitFormButton.addEventListener('click', ()=>{
+  inputs.forEach((item)=>{
+    item.removeAttribute('required');
+  })
+})
+
+async function formSend(e) {
+  e.preventDefault();
+  let error = formValidate(form);
+
+  let formData = new FormData(form);
+
+
+  if (error === 0){
+    // form.classList.add('_sending');
+    let response = await fetch('sendmail.php', {
+      method: 'POST',
+      body: formData
+    });
+    if(response.ok){
+      let result = await response.json();
+      alert(result.message);
+      formPreview.innerHTML = '';
+      form.reset();
+    }else{
+      alert('Ошибка!');
+    }
+  }else{
+    alert('Заполните обязательные поля!');
+  }
+}
+
+function formValidate(form){
+  let error = 0;
+  let formReq = document.querySelectorAll('._req');
+
+  for (let i=0; i<formReq.length; i++){
+    const input = formReq[i];
+    formRemoveError(input);
+
+    if(input.classList.contains('_email')){
+      if(emailTest(input)){
+        formAddError(input);
+        error++;
+      }
+    }else if(input.getAttribute('type') === 'checkbox' && input.checked === false){
+      formAddError(input);
+        error++;
+    }else{
+      if(input.value === ''){
+        formAddError(input);
+        error++;
+      }
+    }
+  }
+  return error;
+}
+
+function formAddError(input){
+  input.parentElement.classList.add('_error');
+  input.classList.add('_error')
+}
+
+function formRemoveError(input){
+  input.parentElement.classList.remove('_error');
+  input.classList.remove('_error');
+}
+
+function emailTest(input){
+  return !/^\w+([\.-]?\w+)*@\w+([\.-]?\.\w{2,8})+$/.test(input.value);
+}
